416a78a55e019aa24a452a92773c09accd71e915
Rosy Tucker <rosy533@gmail.com>
2014-10-27 10:57:25 +0000

    Revert "[Derek && Rosy] Delete pages directory"
    This reverts commit 7cf8bf1f0f3287bb6a8989a7607087ac01ba58f9.

---
+++ storefront/features/pages/homepage.rb
@@ -0,0 +1,17 @@
+module Pages
+    class Homepage < CapybaraPageObject::Page
+      path ''
+    #   # component :header do Components::Header.new find('header') end
+    #   def headers
+    #     Components::Header.new find('header')
+    #   end
+    # end
+end
+
+# module Components
+#   class Header < CapybaraPageObject::Component
+#     def click
+#       click_link source
+#     end
+#   end
+end
