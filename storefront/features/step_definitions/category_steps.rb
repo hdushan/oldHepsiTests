8ac00031c1b481ac9c389904f3659c71ce98fc4d
Duncan Mortimer <dmortime@thoughtworks.com>
2014-10-29 12:09:28 +0000

    [Duncan] #373 Cleaning up steps

--- storefront/features/step_definitions/category_steps.rb
+++ storefront/features/step_definitions/category_steps.rb
@@ -2,6 +2,10 @@ Then(/^I see (\d+) items in discounted products$/) do |items|
   expect(page.all('.discounted-product-container li').count.to_s).to eq items
 end
 
+Then(/^I see (\d+) items in top sellers$/) do |items|
+  expect(page.all('.top-seller-container li').count.to_s).to eq items
+end
+
 Then(/^the first discounted product displays the expected fields$/) do
   first_discounted_product_entry = page.first('.discounted-product-container li')
 
@@ -20,14 +24,6 @@ Then(/^the first top seller product displays the expected fields$/) do
   expect(first_top_seller_entry).to have_selector('.product-image-wrapper')
   expect(first_top_seller_entry).to have_selector('.product-price')
   expect(first_top_seller_entry).to have_selector('.ranking-growth')
-end
-
-Then(/^I see (\d+) items in discounted products/) do |items|
-  expect(page.all('.discounted-product-container li').count.to_s).to eq items
-end
-
-Then(/^I see (\d+) items in top sellers/) do |items|
-  expect(page.all('.top-seller-container li').count.to_s).to eq items
 end
 
 Then(/^I click the second discounted product$/) do
