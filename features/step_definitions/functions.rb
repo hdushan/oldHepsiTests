f2e11b1d78a42d15bed3bf2b5c05e4a5a82345d8
Hans Dushanthakumar <hans@thoughtworks.com>
2014-12-04 15:18:56 +0200

    Moved page helper functions to "functions" library file

--- features/step_definitions/functions.rb
+++ features/step_definitions/functions.rb
@@ -36,3 +36,23 @@ def calculate_discount
   p (i/k)
   return (100 - ((i/k)*100).to_i).to_i
 end
+
+def wait_for_visibility(page, selector, visible)
+  @javascript
+  page.should have_selector(selector, visible: visible)
+end
+
+def all_satisfy(expected)
+  RSpec::Matchers::BuiltIn::All.new(expected)
+end
+
+def prices_to_list(items)
+  items.map { |item|
+    price_from_string(item.find('.product-price').text)
+  }
+end
+
+def price_from_string(string)
+  string.gsub!(/[^0-9 ,]/, '')
+  string.gsub!(/,/, '.').to_f
+end
