b19b7da9d30962f3083228daba67ef1719cd71b4
Alper Mermer <alper.mermer@hepsiburada.com>
2014-12-16 14:12:13 +0200

    Changed data

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -39,6 +39,7 @@ Then(/^I add "([^"]*)" product\(s\) to cart$/) do |arg|
     $p_quantity = i
     fill_in "quantity", :with=> i
     find_by_id("addToCart").click
+    page.should have_selector("#notification", :visible => true)
     page.should have_content("#{i} Ürün başarılı bir şekilde sepete eklenmiştir.")
     page.should have_selector("#notification", visible: :hidden)
   end
