4c7ed783e904c964d7b01ff086fb49f899a80f87
Matt Urbanski <murbanski@thoughtworks.com>
2014-12-17 14:12:28 +0200

    Script needs correct exit code

--- scripts/run-user-journeys.sh
+++ scripts/run-user-journeys.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo killall -9 Xvfb
+killall -9 Xvfb
 
 Xvfb :1 -screen 0 1024x768x24 2>&1 >/dev/null &
 export DISPLAY=:1
@@ -8,3 +8,11 @@ sleep 3
 xhost +
 
 /usr/local/bin/cucumber features --format pretty --format html --out results.html --format junit --out results.xml --tags @journey
+rc=$?
+
+if [[ $rc != 0 ]] ; then
+    killall -9 Xvfb
+    exit $rc
+fi
+
+killall -9 Xvfb
