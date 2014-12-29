6c92e274e9ae7e65ec9c882c639341fdca26f838
mehmet.mecek <mehmet.mecek@hepsiburada.com>
2014-12-29 18:11:25 +0200

    #000 selim run cucumber with sudo

--- scripts/run-user-journeys.sh
+++ scripts/run-user-journeys.sh
@@ -12,7 +12,7 @@ sudo xhost +
 echo "Running tests against http://$1" | /usr/games/cowsay
 
 
-/usr/local/bin/cucumber features"$2" --format pretty --format html --out results.html --format junit --out results.xml --tags @journey host=http://"$1"
+sudo /usr/local/bin/cucumber features"$2" --format pretty --format html --out results.html --format junit --out results.xml --tags @journey host=http://"$1"
 
 rc=$?
 
