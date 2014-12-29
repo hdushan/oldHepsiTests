d15e1052a388f9f8a692c860560778b8c6b8e4fc
mehmet.mecek <mehmet.mecek@hepsiburada.com>
2014-12-29 17:51:29 +0200

    #000 selim script is being run by non sudo user

--- scripts/run-user-journeys.sh
+++ scripts/run-user-journeys.sh
@@ -2,25 +2,24 @@
 
 /usr/local/bin/bundle install --verbose
 
-killall -9 Xvfb
+sudo killall -9 Xvfb
 
-Xvfb :1 -screen 0 1024x768x24 2>&1 >/dev/null &
+sudo Xvfb :1 -screen 0 1024x768x24 2>&1 >/dev/null &
 export DISPLAY=:1
 sleep 3
-xhost +
+sudo xhost +
 
 echo "Running tests against http://$1" | /usr/games/cowsay
 
 
-/usr/local/bin/cucumber features"$2" --format pretty --format html --out results.html --format junit --out results.xml --tags @journey host=http://$1
+/usr/local/bin/cucumber features"$2" --format pretty --format html --out results.html --format junit --out results.xml --tags @journey host=http://"$1"
 
 rc=$?
 
 if [[ $rc != 0 ]] ; then
-    killall -9 Xvfb
+    sudo killall -9 Xvfb
     exit $rc
 fi
 
-chmod -R 777 .
-killall -9 Xvfb
-chown go:go .
+sudo chmod -R 777 .
+sudo killall -9 Xvfb
