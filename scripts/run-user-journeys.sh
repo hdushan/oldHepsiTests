065ab38064767218685ea7dbba798a4c2cdd62dd
mehmet.mecek <mehmet.mecek@hepsiburada.com>
2014-12-29 18:49:49 +0200

    #000 selim chown go:users added

--- scripts/run-user-journeys.sh
+++ scripts/run-user-journeys.sh
@@ -2,24 +2,25 @@
 
 /usr/local/bin/bundle install --verbose
 
-sudo killall -9 Xvfb
+killall -9 Xvfb
 
-sudo Xvfb :1 -screen 0 1024x768x24 2>&1 >/dev/null &
+Xvfb :1 -screen 0 1024x768x24 2>&1 >/dev/null &
 export DISPLAY=:1
 sleep 3
-sudo xhost +
+xhost +
 
 echo "Running tests against http://$1" | /usr/games/cowsay
 
 
-sudo /usr/local/bin/cucumber features"$2" --format pretty --format html --out results.html --format junit --out results.xml --tags @journey host=http://"$1"
+/usr/local/bin/cucumber features"$2" --format pretty --format html --out results.html --format junit --out results.xml --tags @journey host=http://"$1"
 
 rc=$?
 
 if [[ $rc != 0 ]] ; then
-    sudo killall -9 Xvfb
+    killall -9 Xvfb
     exit $rc
 fi
 
-sudo chmod -R 777 .
-sudo killall -9 Xvfb
+chmod -R 777 .
+killall -9 Xvfb
+chown go:users .
