c38f3a34faf06278d0fb039ff9038a54401436e7
Hans Dushanthakumar <hans@thoughtworks.com>
2015-01-12 12:47:01 +0200

    Dont need framebuffer for jmeter tests

--- scripts/run-performance.sh
+++ scripts/run-performance.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
-/usr/local/bin/bundle install --verbose
-
-killall -9 Xvfb
-
-Xvfb :1 -screen 0 1024x768x24 2>&1 >/dev/null &
-export DISPLAY=:1
-sleep 3
-xhost +
+bundle install
 
 echo "xxxxxxxxxx Start jmeter performance test" | /usr/games/cowsay
 bundle exec rake performance[$1]
-
-rc=$?
-
-if [[ $rc != 0 ]] ; then
-    killall -9 Xvfb
-    exit $rc
-fi
-
-chmod -R 777 .
-killall -9 Xvfb
-chown -R go:users .
