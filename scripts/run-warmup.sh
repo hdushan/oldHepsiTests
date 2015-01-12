0794706d92f73a9ce8879b4fe41b86c740b06d97
Hans Dushanthakumar <hans@thoughtworks.com>
2015-01-12 12:38:58 +0200

    No framebuffer for jmeter tests

--- scripts/run-warmup.sh
+++ scripts/run-warmup.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
 
-#!/bin/bash
-
-/usr/local/bin/bundle install --verbose
-
-killall -9 Xvfb
-
-Xvfb :1 -screen 0 1024x768x24 2>&1 >/dev/null &
-export DISPLAY=:1
-sleep 3
-xhost +
+/usr/local/bin/bundle install
 
 echo "xxxxxxxxxx Start jmeter warmup test"
 bundle exec rake warmup[$1]
 
-rc=$?
-
-if [[ $rc != 0 ]] ; then
-    killall -9 Xvfb
-    exit $rc
-fi
-
-chmod -R 777 .
-killall -9 Xvfb
-chown -R go:users .
