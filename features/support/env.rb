97db4976e444aee2a742434fd7e27e9c6b1664c6
Alper Mermer <alper.mermer@hepsiburada.com>
2015-01-14 10:22:02 +0200

    #regression  alper: reverting to qa

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,28 +12,28 @@ require 'rest-client'
 require 'tiny_tds'
 require 'time_diff'
 
-Capybara.app_host = 'http://storefront.test.hepsiburada.com'#'http://localhost:99'
+Capybara.app_host = 'http://storefront.qa.hepsiburada.com'#'http://localhost:99'
 if ENV['host']
   Capybara.app_host = ENV['host']
 end
 
 Capybara.run_server = false
-Capybara.default_driver = :firefox
 Capybara.default_selector = :css
 Capybara.default_wait_time = 60 #default wait time for ajax
 Capybara.ignore_hidden_elements = false #ignore hidden elements when testing, make helpful when you hide or show elements using javascript
 Capybara.save_and_open_page_path = File.expand_path(File.join(File.dirname(__FILE__), "../../screenshots/"))
 
-module Helpers
-  def without_resynchronize
-    page.driver.options[:resynchronize] = false
-    yield
-    page.driver.options[:resynchronize] = true
-  end
-end
+# module Helpers
+#   def without_resynchronize
+#     page.driver.options[:resynchronize] = false
+#     yield
+#     page.driver.options[:resynchronize] = true
+#   end
+# end
 
+Capybara.default_driver = :firefox
 Capybara.register_driver :firefox do |app|
   Capybara::Selenium::Driver.new(app, :browser => :firefox)
 end
 
-World(Capybara::DSL, Helpers)
+World(Capybara::DSL)
